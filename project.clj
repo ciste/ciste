ac77bb3471d7123d2a3c9737181b8b2ba2676c2b
Daniel E. Renfer <duck@kronkltd.net>
2011-09-09 20:56:21 -0400

    upgrade to beta3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "MVC platform for Clojure applications"
   :repositories {"jiksnu-internal" "http://build.jiksnu.com/repository/internal"
                  "jiksnu-snapshots" "http://build.jiksnu.com/repository/snapshots"}
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
                  [org.clojure.contrib/lazy-xml "1.3.0-alpha4"]
                  [org.clojure/tools.logging "0.1.2"]
                  [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]
