016c5f418acc236b1790fa7cc0374c8c1d3003bd
Daniel E. Renfer <duck@kronkltd.net>
2011-09-19 03:26:07 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "MVC platform for Clojure applications"
   :repositories {"jiksnu-internal" "http://build.jiksnu.com/repository/internal"
                  "jiksnu-snapshots" "http://build.jiksnu.com/repository/snapshots"}
-  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
-                 [org.clojure.contrib/lazy-xml "1.3.0-alpha4"]
-                 [org.clojure/tools.logging "0.1.2"]
-                 [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]
-                 [inflections "0.5.2"]
-                 [org.clojure/data.json "0.1.0"]
+  :dependencies [[compojure "0.6.5"]
                  [hiccup "0.3.6"]
-                 [compojure "0.6.5"]]
-  :dev-dependencies [[net.kronkltd/midje "1.3-alpha1"]]
+                 [inflections "0.5.2"]
+                 [org.clojure/clojure "1.3.0-beta3"]
+                 [org.clojure/data.json "0.1.0"]
+                 [org.clojure/tools.logging "0.1.2"]
+                 [org.clojure.contrib/lazy-xml "1.3.0-alpha4"]
+                 [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]]
+  :dev-dependencies [[net.kronkltd/midje "1.3-alpha2-SNAPSHOT"]]
   :exclusions [org.clojure/contrib
                org.slf4j/slf4j-log4j12
                org.slf4j/slf4j-jdk14]
