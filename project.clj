badace74cfb41049995d74730b23b1786991fdf3
Daniel E. Renfer <duck@kronkltd.net>
2011-09-20 15:46:46 -0400

    configure logging

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
   :dependencies [[compojure "0.6.5"]
                  [hiccup "0.3.6"]
                  [inflections "0.5.2"]
+                 [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0-beta3"]
                  [org.clojure/data.json "0.1.0"]
                  [org.clojure/tools.logging "0.1.2"]
                  [org.clojure.contrib/lazy-xml "1.3.0-alpha4"]
-                 [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]]
+                 [org.slf4j/slf4j-simple "1.6.1"]]
   :dev-dependencies [[net.kronkltd/midje "1.3-alpha2-SNAPSHOT"]]
   :exclusions [org.clojure/contrib
                org.slf4j/slf4j-log4j12
