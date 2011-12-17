550e760ab1f6ce3511d7ae5890c0fcd9031b2406
Daniel E. Renfer <duck@kronkltd.net>
2011-12-17 14:45:51 -0500

    remove dependencies

--- project.clj
+++ project.clj
@@ -7,12 +7,10 @@
                  [compojure "0.6.5"]
                  [inflections "0.6.3"]
                  [lamina "0.4.1-SNAPSHOT"]
-                 [midje "1.3-alpha5"]
-                 [org.clojure/algo.generic "0.1.0"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [potemkin "0.1.0"]]
+                 [org.clojure/tools.logging "0.2.3"]]
+  :dev-dependencies [[midje "1.3.0" :exclusions [org.clojure/clojure]]]
   :warn-on-reflection false
   :jvm-opts ["-server"])
