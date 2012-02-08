9c78eaed3ca496e45564702a69793733c8021a19
Daniel E. Renfer <duck@kronkltd.net>
2012-02-08 18:52:29 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "MVC platform for Clojure applications"
   :url "http://github.com/duck1123/ciste"
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[clj-factory "0.2.0-SNAPSHOT"]
+  :dependencies [[clj-factory "0.2.0"]
                  [clj-stacktrace "0.2.3"]
                  [compojure "0.6.5"]
                  [inflections "0.6.3"]
-                 [lamina "0.4.1-SNAPSHOT"]
+                 [lamina "0.4.1-alpha1"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
