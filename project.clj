0f8463690e1449d6f967be43faebfb6aada294fe
Daniel E. Renfer <duck@kronkltd.net>
2012-02-09 21:14:33 -0500

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ciste "0.2.0"
+(defproject ciste "0.3.0-SNAPSHOT"
   :description "MVC platform for Clojure applications"
   :url "http://github.com/duck1123/ciste"
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
