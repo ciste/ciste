de52d040626e9c84796b9a43a9db09420d3599d2
Daniel E. Renfer <duck@kronkltd.net>
2012-02-11 19:59:03 -0500

    runner gives error message if service config can not be found

--- src/ciste/runner.clj
+++ src/ciste/runner.clj
@@ -1,30 +1,40 @@
 (ns ciste.runner
   (:use (ciste [config :only [load-config set-environment!]]
                [debug :only [spy]]))
-  (:require (clojure.tools [logging :as log])))
+  (:require (clojure.tools [logging :as log]))
+  (:import java.io.FileNotFoundException))
 
 (defn read-site-config
   []
+  ;; TODO: Check a variety of places for this file.
   (-> "ciste.clj" slurp read-string))
 
 (defn -main
   [& options]
-  (let [opts (apply hash-map options)
-        environment :development
-        service-config (read-site-config)]
-    (load-config)
+  (let [opts (apply hash-map options)]
+    (try
+      (let [service-config (read-site-config)
 
-    (doseq [sn (:services service-config)]
-      (require (symbol sn)))
+            ;; TODO: allow this to be passed in via command line
+            environment (:environment service-config)]
 
-    (log/info (str "Starting service in " environment " mode."))
+        ;; TODO: initialize config backend
+        (load-config)
 
-    ;; TODO: Also read environment variable
-    (set-environment! (:environment service-config))
-
-    (doseq [service-name (:services service-config)]
-      (log/info (str "Loading " service-name))
-      ((intern (the-ns (symbol service-name)) (symbol "start"))))
-
-    ;; TODO: store this and allow it for shutdown.
-    @(promise)))
+        ;; TODO: load namespaces other than services to be started
+        (doseq [sn (:services service-config)]
+          (log/debug (str "Loading " sn))
+          (require (symbol sn)))
+        
+        (log/info (str "Starting service in " environment " mode."))
+        (set-environment! environment)
+        
+        (doseq [service-name (:services service-config)]
+          (log/info (str "Starting " service-name))
+          ((intern (the-ns (symbol service-name)) (symbol "start"))))
+        
+        ;; TODO: store this and allow it for shutdown.
+        @(promise))
+      (catch FileNotFoundException ex
+        (log/error "Could not find service config. Ensure that ciste.clj exists at the root of your application and is readable")
+        (System/exit -1)))))
