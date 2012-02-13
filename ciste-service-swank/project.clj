e6ad37328bfb65c906d966d13e8ee7119fcdc373
Daniel E. Renfer <duck@kronkltd.net>
2012-02-13 02:27:29 -0500

    move to modular layout

---
+++ ciste-service-swank/project.clj
@@ -0,0 +1,6 @@
+(defproject ciste/ciste-service-swank "0.3.0-SNAPSHOT"
+  :description "Swank Connector Service for Ciste Applications"
+  :url "http://github.com/duck1123/ciste"
+  :author "Daniel E. Renfer <duck@kronkltd.net>"
+  :dependencies [[ciste/ciste-core "0.3.0-SNAPSHOT"]
+                 [swank-clojure "1.4.0"]])
--- project.clj
+++ project.clj
@@ -2,19 +2,13 @@
   :description "MVC platform for Clojure applications"
   :url "http://github.com/duck1123/ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[aleph "0.2.1-alpha1"]
-                 [clj-factory "0.2.0"]
-                 [clj-stacktrace "0.2.4"]
-                 [compojure "1.0.1"]
-                 [inflections "0.6.5"]
-                 [org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.incubator "0.1.0"]
-                 [org.clojure/data.json "0.1.2"]
-                 [org.clojure/tools.logging "0.2.3"]]
-  :dev-dependencies [[lein-midje "1.0.8"]
-                     [log4j "1.2.16"]
-                     [midje "1.3.2-alpha1" :exclusions [org.clojure/clojure]]]
+  :dependencies [[ciste/ciste-core "0.3.0-SNAPSHOT"]
+                 [ciste/ciste-service-swank "0.3.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-sub "0.1.2"]]
+  :sub [
+        "ciste-core"
+        "ciste-service-swank"
+        ]
   :autodoc {:name "Ciste"
             :copyright "2012 KRONK Ltd."}
   :warn-on-reflection false
