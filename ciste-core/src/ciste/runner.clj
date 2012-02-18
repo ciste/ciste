ca3f52e61c8ab182b50f6039e6f0711b7bf438aa
Daniel E. Renfer <duck@kronkltd.net>
2012-02-18 14:18:05 -0500

    runner support

--- ciste-core/src/ciste/runner.clj
+++ ciste-core/src/ciste/runner.clj
@@ -6,35 +6,35 @@
 
 (defn read-site-config
   []
-  ;; TODO: Check a variety of places for this file.
-  (-> "ciste.clj" slurp read-string))
+  (try
+    ;; TODO: Check a variety of places for this file.
+    (-> "ciste.clj" slurp read-string)
+    (catch FileNotFoundException ex
+      ;; TODO: Throw an exception here
+      (log/error "Could not find service config. Ensure that ciste.clj exists at the root of your application and is readable")
+      (System/exit -1))))
 
 (defn -main
   [& options]
-  (let [opts (apply hash-map options)]
-    (try
-      (let [service-config (read-site-config)
+  (let [opts (apply hash-map options)
+        service-config (read-site-config)
 
-            ;; TODO: allow this to be passed in via command line
-            environment (:environment service-config)]
+        ;; TODO: allow this to be passed in via command line
+        environment (:environment service-config)]
+    ;; TODO: initialize config backend
+    (load-config)
 
-        ;; TODO: initialize config backend
-        (load-config)
-
-        ;; TODO: load namespaces other than services to be started
-        (doseq [sn (:services service-config)]
-          (log/debug (str "Loading " sn))
-          (require (symbol sn)))
-        
-        (log/info (str "Starting service in " environment " mode."))
-        (set-environment! environment)
-        
-        (doseq [service-name (:services service-config)]
-          (log/info (str "Starting " service-name))
-          ((intern (the-ns (symbol service-name)) (symbol "start"))))
-        
-        ;; TODO: store this and allow it for shutdown.
-        @(promise))
-      (catch FileNotFoundException ex
-        (log/error "Could not find service config. Ensure that ciste.clj exists at the root of your application and is readable")
-        (System/exit -1)))))
+    ;; TODO: load namespaces other than services to be started
+    (doseq [sn (:services service-config)]
+      (log/debug (str "Loading " sn))
+      (require (symbol sn)))
+    
+    (log/info (str "Starting service in " environment " mode."))
+    (set-environment! environment)
+    
+    (doseq [service-name (:services service-config)]
+      (log/info (str "Starting " service-name))
+      ((intern (the-ns (symbol service-name)) (symbol "start"))))
+    
+    ;; TODO: store this and allow it for shutdown.
+    @(promise)))
