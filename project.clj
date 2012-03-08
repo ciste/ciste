e7ee6939ca7c598d8969ec666ff4fdf3d4ed831a
Daniel E. Renfer <duck@kronkltd.net>
2012-03-08 18:39:35 -0500

    tigase service

--- project.clj
+++ project.clj
@@ -2,16 +2,20 @@
   :description "MVC platform for Clojure applications"
   :url "http://github.com/duck1123/ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
-  :dependencies [[ciste/ciste-core "0.3.0-SNAPSHOT"]
+  :dependencies [
+                 [ciste/ciste-core "0.3.0-SNAPSHOT"]
                  [ciste/ciste-service-aleph "0.3.0-SNAPSHOT"]
                  [ciste/ciste-service-slacker "0.3.0-SNAPSHOT"]
-                 [ciste/ciste-service-swank "0.3.0-SNAPSHOT"]]
+                 [ciste/ciste-service-swank "0.3.0-SNAPSHOT"]
+                 [ciste/ciste-service-tigase "0.3.0-SNAPSHOT"]
+                 ]
   :dev-dependencies [[lein-sub "0.1.2"]]
   :sub [
         "ciste-core"
         "ciste-service-aleph"
         "ciste-service-slacker"
         "ciste-service-swank"
+        "ciste-service-tigase"
         ]
   :autodoc {:name "Ciste"
             :copyright "2012 KRONK Ltd."}
