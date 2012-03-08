5c37c74ae157826e8168422be88f402da869b1a6
Daniel E. Renfer <duck@kronkltd.net>
2012-03-08 14:31:17 -0500

    aleph module

--- project.clj
+++ project.clj
@@ -3,11 +3,13 @@
   :url "http://github.com/duck1123/ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :dependencies [[ciste/ciste-core "0.3.0-SNAPSHOT"]
+                 [ciste/ciste-service-aleph "0.3.0-SNAPSHOT"]
                  [ciste/ciste-service-slacker "0.3.0-SNAPSHOT"]
                  [ciste/ciste-service-swank "0.3.0-SNAPSHOT"]]
   :dev-dependencies [[lein-sub "0.1.2"]]
   :sub [
         "ciste-core"
+        "ciste-service-aleph"
         "ciste-service-slacker"
         "ciste-service-swank"
         ]
