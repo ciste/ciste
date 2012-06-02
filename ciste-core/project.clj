57ad981522d110f1181c72ecfa067ce207a9757e
Daniel E. Renfer <duck@kronkltd.net>
2012-06-01 20:04:42 -0400

    checkpoint

--- ciste-core/project.clj
+++ ciste-core/project.clj
@@ -3,30 +3,24 @@
   :url "http://github.com/duck1123/ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [
-                 [clj-factory "0.2.0"]
-                 [clj-http "0.3.2"]
-                 [clj-stacktrace "0.2.4"]
+  :dependencies [[clj-factory "0.2.0"]
+                 [clj-http "0.4.2"]
+                 ;; [clj-stacktrace "0.2.4"]
                  [compojure "1.0.1"]
                  [enlive "1.0.0"]
                  [inflections "0.6.5"]
-                 [lamina "0.5.0-alpha2"]
+                 [lamina "0.5.0-alpha3"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [xml-picker-seq "0.0.2"]
-                 ]
+                 [xml-picker-seq "0.0.2"]]
   :profiles {:dev
              {:dependencies
-              [
-               [log4j "1.2.16"]
-               [midje "1.4.0-beta2"]
-               ]}}
+              [[log4j "1.2.16"]
+               [midje "1.4.0"]]}}
 
-  :plugins [
-            [lein-midje "2.0.0-SNAPSHOT"]
-            ]
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
   :autodoc {:name "Ciste"
             :copyright "2012 KRONK Ltd."}
   :warn-on-reflection false
---
+++ ciste-service-aleph/project.clj
@@ -0,0 +1,7 @@
+(defproject ciste/ciste-service-aleph "0.3.0-SNAPSHOT"
+  :description "Aleph Connector Service for Ciste Applications"
+  :url "http://github.com/duck1123/ciste"
+  :author "Daniel E. Renfer <duck@kronkltd.net>"
+  :dependencies [[aleph "0.3.0-alpha2"]
+                 [ciste/ciste-core "0.3.0-SNAPSHOT"]]
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
