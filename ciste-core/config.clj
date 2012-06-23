1d28a7891c3a63196808e211a72a0a72da009337
Daniel E. Renfer <duck@kronkltd.net>
2012-06-22 23:53:29 -0400

    add required files for testing

---
+++ ciste-core/config.clj
@@ -0,0 +1,10 @@
+{:development
+ {:print {:predicates false}
+  :triggers {:thread-count 1}}
+
+ :test
+ {:print {:actions    true
+          :predicates false}
+  :run-triggers true
+  :triggers {:thread-count 1}
+  :use-pipeline true}}
