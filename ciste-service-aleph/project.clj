e49de7822db57529083df09da136ef4875c021b0
Daniel E. Renfer <duck@kronkltd.net>
2012-07-10 21:13:02 -0400

    remove aleph service

--- ciste-service-aleph/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject ciste/ciste-service-aleph "0.4.0-SNAPSHOT"
-  :description "Aleph Connector Service for Ciste Applications"
-  :url "http://github.com/duck1123/ciste"
-  :author "Daniel E. Renfer <duck@kronkltd.net>"
-  :min-lein-version "2.0.0"
-  :dependencies [[aleph "0.3.0-alpha2"]
-                 [ciste/ciste-core "0.4.0-SNAPSHOT"]]
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
