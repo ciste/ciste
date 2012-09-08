0b1a0df2a44ac0a4d6614f5f197b8164fd9e54df
Daniel E. Renfer <duck@kronkltd.net>
2012-09-07 22:15:03 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -6,24 +6,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[clj-factory "0.2.0"]
-                 [clj-http "0.4.4"]
-                 [compojure "1.1.0"]
-                 [enlive "1.0.0"]
-                 [inflections "0.6.5"]
+                 [clj-http "0.5.3"]
+                 [compojure "1.1.3"]
+                 [enlive "1.0.1"]
+                 [inflections "0.7.2"]
                  [lamina "0.5.0-alpha3"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.1"]
-                 [org.clojure/data.json "0.1.2"]
-                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/data.json "0.1.3"]
+                 [org.clojure/tools.logging "0.2.4"]
                  [xom "1.2.5"]]
   :profiles {:dev
              {:dependencies
-              [[log4j "1.2.16"]
+              [[log4j "1.2.17"]
                [midje "1.4.0"]]}}
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]
             [codox "0.6.1"]]
   :autodoc {:name "Ciste"
-            :copyright "2012 KRONK Ltd."}
-  :warn-on-reflection false
-  :jvm-opts ["-server"]
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
+            :copyright "2012 KRONK Ltd."})
