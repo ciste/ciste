a514300b089ec0a62b58196b346853c927af3e1e
Daniel E. Renfer <duck@kronkltd.net>
2012-10-08 22:08:05 -0400

    bump dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[clj-factory "0.2.0"]
+  :dependencies [[clj-factory "0.2.1"]
                  [clj-http "0.5.5"]
                  [compojure "1.1.3"]
                  [enlive "1.0.1"]
