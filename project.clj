678ff174ba0e3af03b6e3fd8516a8a40f060daee
Daniel E. Renfer <duck@kronkltd.net>
2012-10-08 21:53:56 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[clj-factory "0.2.0"]
-                 [clj-http "0.5.3"]
+                 [clj-http "0.5.5"]
                  [compojure "1.1.3"]
                  [enlive "1.0.1"]
-                 [inflections "0.7.2"]
+                 [inflections "0.7.3"]
                  [lamina "0.5.0-alpha3"]
                  [org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.incubator "0.1.1"]
+                 [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/data.json "0.1.3"]
                  [org.clojure/tools.logging "0.2.4"]
                  [xom "1.2.5"]]
