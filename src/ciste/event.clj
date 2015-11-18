55bd5046a60d84406360baa5d9f58a7f119d7315
Daniel E. Renfer <duck@kronkltd.net>
2015-11-18 07:59:25 -0500

    Ensure that emitter and keys are only defined once

--- src/ciste/event.clj
+++ src/ciste/event.clj
@@ -2,17 +2,17 @@
   (:require [clojurewerkz.eep.emitter :as e]
             [slingshot.slingshot :refer [throw+]]))
 
-(def ^:dynamic *keys* (ref {}))
+(defonce ^:dynamic *keys* (ref {}))
 
-(def emitter
+(defonce emitter
   (e/create {:dispatcher-type :ring-buffer}))
 
 (defn notify
   [channel-key msg]
   ;; TODO: make sure that the channel is reqistered
   (if-let [d (get @*keys* channel-key)]
-    (e/notify emitter key (assoc msg :event channel-key))
-    (throw+ (str "No description for key: " channel-key))))
+    (e/notify emitter channel-key (assoc msg :event channel-key))
+    (throw+ {:msg (str "No description for key: " channel-key)})))
 
 (defn defkey
   [channel-key desc & {:as body}]
