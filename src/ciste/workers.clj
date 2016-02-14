25940b20a71f6a606d4a78f36ee7994910e6176d
Daniel E. Renfer <duck@kronkltd.net>
2016-02-13 21:50:47 -0500

    Add additional logging

--- src/ciste/workers.clj
+++ src/ciste/workers.clj
@@ -69,12 +69,14 @@
           (loop []
             (try
               (apply inner-fn name args)
-              (catch Exception e (timbre/error e "Uncaught exception")))
+              (catch Exception ex
+                (timbre/error ex "Uncaught exception")))
             (if-let [stopping (stopping? id)]
               (timbre/debugf "(stopping? %s %s): %s" name id stopping)
               (do (Thread/sleep (config :worker-timeout))
                   (recur)))))
-        (catch Exception e)
+        (catch Exception ex
+          (timbre/error ex "Uncaught exception"))
         (finally
           (timbre/infof "Worker %s (%s) finished" name id)
           (dosync
