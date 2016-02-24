baa849e4798bc350a06987efab5a6112eb0bb3f5
Daniel E. Renfer <duck@kronkltd.net>
2016-02-23 21:31:06 -0500

    add clojure maven plugin to pom

--- project.clj
+++ project.clj
@@ -5,24 +5,52 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :cljsbuild {:builds []}
   :dependencies [[clj-factory "0.2.1"]
-                 [clj-http "3.4.1"]
+                 [clj-http "3.3.0"]
                  [clojurewerkz/propertied "1.2.0"]
-                 [com.taoensso/timbre "4.8.0"]
+                 [com.taoensso/timbre "4.7.4"]
                  [compojure "1.5.1"]
                  [enlive "1.1.6"]
                  [environ "1.1.0"]
                  [inflections "0.12.2" :exclusions [commons-codec]]
                  [manifold "0.1.5"]
-                 [org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [xom "1.2.5"]]
-  :profiles {:dev {:dependencies [[midje "1.9.0-alpha6"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.22"]]}}
+  :profiles {:dev {:dependencies [[log4j "1.2.17"]
+                                  [midje "1.8.3"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.21"]]}}
   :auto-clean false
   ;; :aot [ciste.runner]
   :plugins [[lein-midje "3.1.3"]
             [codox "0.8.11"]]
+  :pom-plugins [[org.apache.maven.plugins/maven-jar-plugin "2.6"
+                 {:executions ([:execution
+                                [:id "default-jar"]
+                                [:phase "never"]
+                                [:configuration
+                                 [:finalName "unwanted"]
+                                 [:classifier "unwanted"]]])}]
+                [org.codehaus.mojo/build-helper-maven-plugin "1.10"
+                 {:executions ([:execution
+                                [:id "attach-artifacts"]
+                                [:phase "package"]
+                                [:goals
+                                 [:goal "attach-artifact"]
+                                 ]
+                                [:configuration
+                                 [:artifacts
+                                  [:artifact
+                                   [:file "target/ciste-0.6.0-SNAPSHOT.jar"]
+                                   [:type "jar"]
+                                   [:classifier "compile"]]]]])}]]
+  :pom-addition ([:properties
+                  [:project.build.sourceEncoding "UTF-8"]])
+  :repositories [["snapshots" {:url "http://repo.jiksnu.org/repository/maven-snapshots/"
+                               :username [:gpg :env/repo_username]
+                               :password [:gpg :env/repo_password]}]
+                 ["releases" {:url "http://repo.jiksnu.org/repository/maven-releases/"
+                              :username [:gpg :env/repo_username]
+                              :password [:gpg :env/repo_password]}]]
   :autodoc {:name "Ciste"
             :copyright "2014 KRONK Ltd."})
